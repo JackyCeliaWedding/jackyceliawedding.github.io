e8552e06ff35e9814657516c16db3580cb08cce4
Soar Lin <soar.lin@gmail.com>
2019-01-06 22:53:19 +0800

    Modify version 1.0

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // navigation click actions
-  $('.scroll-link').on('click', function(event){
+  $('.scroll-link').on('click', function (event) {
     event.preventDefault();
     var sectionID = $(this).attr("data-id");
     scrollToID('#' + sectionID, 750);
   });
   // scroll to top action
-  $('.scroll-top').on('click', function(event) {
+  $('.scroll-top').on('click', function (event) {
     event.preventDefault();
-    $('html, body').animate({scrollTop:0}, 'slow');
+    $('html, body').animate({
+      scrollTop: 0
+    }, 'slow');
   });
   // mobile nav toggle
   $('#nav-toggle').on('click', function (event) {
@@ -17,34 +19,38 @@ $(document).ready(function() {
   });
 
   // Swiper
-  var mySwiper = new Swiper ('.swiper-container', {
+  var mySwiper = new Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
     paginationClickable: true,
+    touchReleaseOnEdges: true,
+    grabCursor: true,
     loop: true,
     autoplay: 3200,
     speed: 800,
     autoplayDisableOnInteraction: false,
   });
 
-  $('#refill').on('click', function(event){
+  $('#refill').on('click', function (event) {
     $('#rsvp_form').show();
     $('#refill').hide();
   });
 
 });
 // scroll function
-function scrollToID(id, speed){
+function scrollToID(id, speed) {
   var offSet = 50;
   var targetOffset = $(id).offset().top - offSet;
   var mainNav = $('#main-nav');
-  $('html,body').animate({scrollTop:targetOffset}, speed);
+  $('html,body').animate({
+    scrollTop: targetOffset
+  }, speed);
   if (mainNav.hasClass("open")) {
     mainNav.css("height", "1px").removeClass("in").addClass("collapse");
     mainNav.removeClass("open");
   }
 }
 if (typeof console === "undefined") {
-    console = {
-        log: function() { }
-    };
+  console = {
+    log: function () {}
+  };
 }
